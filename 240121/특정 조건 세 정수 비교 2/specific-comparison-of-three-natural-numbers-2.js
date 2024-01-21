42c5cb59163d360c514d0f20f11f967d5a69dd3b
Hoonseok <56246060+hoongding@users.noreply.github.com>
2024-01-21 14:44:25 +0900

    Title: 특정 조건 세 정수 비교 2, Time: 175ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240121/특정 조건 세 정수 비교 2/specific-comparison-of-three-natural-numbers-2.js
+++ 240121/특정 조건 세 정수 비교 2/specific-comparison-of-three-natural-numbers-2.js
@@ -3,6 +3,6 @@ const fs = require("fs");
 const [a, b ,c] = fs.readFileSync(0).toString().split(' ');
 
 const firstCompare = b > a && b > c;
-const secondCompare = b === a && b === c;
+const secondCompare = b == a && b == c;
 
 console.log(`${+firstCompare} ${+secondCompare}`);
