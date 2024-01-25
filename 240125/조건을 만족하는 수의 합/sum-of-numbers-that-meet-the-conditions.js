4b36f5538cbcad0fa0c3f0dbf0fd7f35f75b81f4
Hoonseok <56246060+hoongding@users.noreply.github.com>
2024-01-25 17:34:27 +0900

    Title: 조건을 만족하는 수의 합, Time: 198ms, Memory: 47MB, Status: Passed - CodeTree

--- 240125/조건을 만족하는 수의 합/sum-of-numbers-that-meet-the-conditions.js
+++ 240125/조건을 만족하는 수의 합/sum-of-numbers-that-meet-the-conditions.js
@@ -4,6 +4,6 @@ const number = Number(fs.readFileSync(0).toString().trim());
 
 let answer = 0;
 
-for (let i = number; i <= 500; i += 2) answer += i;
+for (let i = number % 2 === 0 ? number : number + 1; i <= 500; i += 2) answer += i;
 
 console.log(answer);
