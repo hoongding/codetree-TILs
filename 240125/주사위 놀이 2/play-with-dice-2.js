6a14c1e51a19ebe375cbd6f0e89094e841f60b34
Hoonseok <56246060+hoongding@users.noreply.github.com>
2024-01-25 17:43:50 +0900

    Title: 주사위 놀이 2, Time: 174ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240125/주사위 놀이 2/play-with-dice-2.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+
+const inputs = fs.readFileSync(0).toString().split('\n');
+
+const numbers = inputs[1].split(' ').map((str) => Number(str));
+
+const diceNum = {
+    1: 0,
+    2: 0,
+    3: 0,
+    4: 0,
+    5: 0,
+    6: 0,
+    7: 0,
+    8: 0,
+}
+
+numbers.forEach((number) => {
+    diceNum[number] += 1;
+})
+
+const diceNumKeys = Object.keys(diceNum);
+
+diceNumKeys.forEach((key) => console.log(`${key} - ${diceNum[key]}`));
