5dab944752869813c56e1c2e3115821c4408f37d
Hoonseok <56246060+hoongding@users.noreply.github.com>
2024-01-26 20:12:14 +0900

    Title: 특정 조건 세 정수 비교, Time: 175ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240126/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+
+const [a, b, c] = fs.readFileSync(0).toString().trim().split(' ').map(str => + str);
+
+const firstAnswer = (a, b, c) => a === Math.min[a, b, c] ? 1 : 0;
+const secondAnswer = (a, b, c) => a === b && a === c ? 1 : 0;
+
+console.log(firstAnswer(a, b, c), secondAnswer(a, b, c));
