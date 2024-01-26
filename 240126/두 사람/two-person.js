a254434718de10abaee2867f38d996e9229552b2
Hoonseok <56246060+hoongding@users.noreply.github.com>
2024-01-26 21:38:46 +0900

    Title: 두 사람, Time: 296ms, Memory: 14MB, Status: Passed - CodeTree

--- 240126/두 사람/two-person.js
+++ 240126/두 사람/two-person.js
@@ -2,8 +2,8 @@ const fs = require('fs');
 
 const peopleInfo = fs.readFileSync(0).toString().trim().split('\n');
 
-const [aAge, aGender] = peopleInfo[0];
-const [bAge, bGender] = peopleInfo[1];
+const [aAge, aGender] = peopleInfo[0].split(' ');
+const [bAge, bGender] = peopleInfo[1].split(' ');
 
 const checkCondition = (age, gender) => Number(age) >= 19 && gender === 'M';
 
