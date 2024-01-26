bc82dec6b0b6a1d0bb093475a9ecb3f912941d2a
Hoonseok <56246060+hoongding@users.noreply.github.com>
2024-01-26 16:12:46 +0900

    Title: 입력받아 연산하기, Time: 181ms, Memory: 15MB, Status: Passed - CodeTree

--- 240126/입력받아 연산하기/take-input-and-operate.js
+++ 240126/입력받아 연산하기/take-input-and-operate.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-const [a, b] = fs.readFileSync(0).toString().trim().split(' ').map(str => Number(str));
+const [a, b] = fs.readFileSync(0).toString().trim().split('\n').map(str => Number(str));
 
 console.log(a + 87);
 console.log(b % 10);
