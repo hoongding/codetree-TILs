4d9c952b3c22df68a607ed5a837348978491629c
Hoonseok <56246060+hoongding@users.noreply.github.com>
2024-01-26 22:35:55 +0900

    Title: 정수 입력받아 배수 출력, Time: 210ms, Memory: 47MB, Status: Passed - CodeTree

---
+++ 240126/정수 입력받아 배수 출력/print-multiple-of-input.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+
+const n = Number(fs.readFileSync(0).toString().trim());
+
+let answer = '';
+for (let i = n; i <= n * 5; i += n) answer += `${i} `;
+
+console.log(answer.trim());
