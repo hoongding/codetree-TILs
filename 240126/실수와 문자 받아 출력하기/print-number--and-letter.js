ff636ea728459c8d84731efd20334ea619bbd3a0
Hoonseok <56246060+hoongding@users.noreply.github.com>
2024-01-26 15:40:30 +0900

    Title: 실수와 문자 받아 출력하기, Time: 180ms, Memory: 14MB, Status: Passed - CodeTree

--- 240126/실수와 문자 받아 출력하기/print-number--and-letter.js
+++ 240126/실수와 문자 받아 출력하기/print-number--and-letter.js
@@ -5,5 +5,5 @@ const [c, a, b] = fs.readFileSync(0).toString().trim().split('\n');
 const roundTwoDigit = (number) => (Math.round(number * 100) / 100).toFixed(2);
 
 [c, a, b].forEach((variable) => {
-    Number(variable) !== NaN ? console.log(roundTwoDigit(variable)) : console.log(variable);
+    Number(variable) ? console.log(roundTwoDigit(variable)) : console.log(variable);
 })
