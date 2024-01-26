5c1eff474dd50aaa2d0b8db00842bfe02bf43696
Hoonseok <56246060+hoongding@users.noreply.github.com>
2024-01-26 20:56:40 +0900

    Title: 장학금, Time: 191ms, Memory: 14MB, Status: Passed - CodeTree

--- 240126/장학금/scholarship.js
+++ 240126/장학금/scholarship.js
@@ -3,6 +3,6 @@ const fs = require('fs');
 const [midterm, final] = fs.readFileSync(0).toString().trim().split(' ').map(str => +str);
 
 if(midterm < 90) console.log(0);
-else if(final >= 95) console.log(10);
-else if(final >= 90) console.log(5);
+else if(final >= 95) console.log(100000);
+else if(final >= 90) console.log(50000);
 else console.log(0);
