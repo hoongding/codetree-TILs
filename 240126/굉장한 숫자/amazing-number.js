9817493805ad2d5a39b727c13fa21627c05c57d8
Hoonseok <56246060+hoongding@users.noreply.github.com>
2024-01-26 20:48:43 +0900

    Title: 굉장한 숫자, Time: 208ms, Memory: 15MB, Status: Passed - CodeTree

--- 240126/굉장한 숫자/amazing-number.js
+++ 240126/굉장한 숫자/amazing-number.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 const n = Number(fs.readFileSync(0).toString().trim());
 
-const firstCondition = (number) => number % 2 === 1 || number % 3 === 0;
-const secondCondition = (number) => number % 2 === 0 || number % 5 === 0;
+const firstCondition = (number) => number % 2 === 1 && number % 3 === 0;
+const secondCondition = (number) => number % 2 === 0 && number % 5 === 0;
 
 console.log(firstCondition(n) || secondCondition(n));
