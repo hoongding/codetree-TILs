f3623e877babfdeb84d43fb8df873c08eeb3b335
Hoonseok <56246060+hoongding@users.noreply.github.com>
2024-01-26 19:34:44 +0900

    Title: 두 숫자의 짝홀 여부, Time: 191ms, Memory: 47MB, Status: Passed - CodeTree

--- 240126/두 숫자의 짝홀 여부/parity-of-two-numbers.js
+++ 240126/두 숫자의 짝홀 여부/parity-of-two-numbers.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-const [a, b] = fs.readFileSync(1).toString().trim().split(' ').map(str => +str);
+const [a, b] = fs.readFileSync(0).toString().trim().split(' ').map(str => +str);
 
 const evenOrOdd = (number) => {
     console.log(number % 2 === 0 ? 'even' : 'odd');
