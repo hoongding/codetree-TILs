e27837b9549c6773b930eec5646b990d1f8050c6
Hoonseok <56246060+hoongding@users.noreply.github.com>
2024-01-26 16:19:49 +0900

    Title: 합과 평균, Time: 167ms, Memory: 15MB, Status: Passed - CodeTree

--- 240126/합과 평균/sum-and-avg.js
+++ 240126/합과 평균/sum-and-avg.js
@@ -2,4 +2,4 @@ const fs = require('fs');
 
 const [a, b] = fs.readFileSync(0).toString().trim().split(' ').map(str => Number(str));
 
-console.log(a + b, (a + b) / 2);
+console.log(a + b, (Math.round((a + b) / 2 * 10) / 10).toFixed(1));
