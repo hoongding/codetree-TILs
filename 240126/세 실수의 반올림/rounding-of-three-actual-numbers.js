fea53e7919d90467ab957ded8eaf9222a2823f5f
Hoonseok <56246060+hoongding@users.noreply.github.com>
2024-01-26 15:27:58 +0900

    Title: 세 실수의 반올림, Time: 186ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240126/세 실수의 반올림/rounding-of-three-actual-numbers.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+
+const [a, b, c] = fs.readFileSync(0).toString().trim().split('\n').map(str => Number(str));
+
+const roundThreeDigit = (number) => (Math.round(number * 1000) / 1000).toString(3);
+
+[a, b, c].forEach((number) => console.log(roundThreeDigit(number)));
