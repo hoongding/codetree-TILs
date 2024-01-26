e5f80badbcd2701425039364c8bfc2779e72e486
Hoonseok <56246060+hoongding@users.noreply.github.com>
2024-01-26 17:29:54 +0900

    Title: 출석 부르기, Time: 169ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240126/출석 부르기/calling-attendance.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+
+const studentNumber = Number(fs.readFileSync(0).toString().trim());
+
+const attendance = {
+    1: 'John',
+    2: 'Tom',
+    3: 'Paul',
+}
+
+console.log(attendance[studentNumber]);
