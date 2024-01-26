64906ee81e4db68847544c3e7f21bfb84310f777
Hoonseok <56246060+hoongding@users.noreply.github.com>
2024-01-26 15:50:23 +0900

    Title: 1시간 뒤 시간 출력, Time: 328ms, Memory: 14MB, Status: Passed - CodeTree

--- 240126/1시간 뒤 시간 출력/print-one-hour-later.js
+++ 240126/1시간 뒤 시간 출력/print-one-hour-later.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
 
-let [hour, minute] = fs.readFileSync(0).toString().trim().split(':');
+let [hour, minute] = fs.readFileSync(0).toString().trim().split(':').map(str => Number(str));
 
 console.log(`${hour + 1}:${minute}`);
