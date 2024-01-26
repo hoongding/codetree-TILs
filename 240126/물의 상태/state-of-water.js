e14911e955b3ba2bd3ebc71eca7ca8cd4aaf8136
Hoonseok <56246060+hoongding@users.noreply.github.com>
2024-01-26 17:18:23 +0900

    Title: 물의 상태, Time: 205ms, Memory: 14MB, Status: Passed - CodeTree

--- 240126/물의 상태/state-of-water.js
+++ 240126/물의 상태/state-of-water.js
@@ -4,7 +4,7 @@ const temparature = Number(fs.readFileSync(0).toString().trim());
 
 let answer = '';
 
-if (temparature > 0) answer = 'ice';
+if (temparature < 0) answer = 'ice';
 else if(temparature >= 100) answer = 'vapor';
 else answer = 'water';
 
