5dcc5ff300952167856b0f8061adf02651ef71d9
Hoonseok <56246060+hoongding@users.noreply.github.com>
2024-01-26 17:39:55 +0900

    Title: 살 수 있는 물건 2, Time: 221ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240126/살 수 있는 물건 2/things-able-to-buy-2.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+
+const n = Number(fs.readFileSync(0).toString().trim());
+
+let answer = ''
+
+if(n >= 3000) answer = 'book';
+else if(n >= 1000) answer = 'mask';
+else if(n >= 500) answer = 'pen';
+else answer = 'no';
+
+console.log(answer);
