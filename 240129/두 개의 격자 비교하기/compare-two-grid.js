65ea1de1c60b60200a7b90ee7950b606e74c44df
Hoonseok <56246060+hoongding@users.noreply.github.com>
2024-01-29 21:25:51 +0900

    Title: 두 개의 격자 비교하기, Time: 200ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240129/두 개의 격자 비교하기/compare-two-grid.js
+++ 240129/두 개의 격자 비교하기/compare-two-grid.js
@@ -8,8 +8,6 @@ const numbers = inputs.slice(1);
 const squreA = numbers.slice(0, n).map((line) => line.split(' ').map(str => +str));
 const squreB = numbers.slice(n, numbers.length).map((line) => line.split(' ').map(str => +str));
 
-const newSquare = squreA.map((line, rowIndex) => line.map((number, colIndex) => {
-    return number == squreB[rowIndex][colIndex] ? 0 : 1;
-}))
+const newSquare = squreA.map((line, rowIndex) => line.map((number, colIndex) => number === squreB[rowIndex][colIndex] ? 0 : 1))
 
-newSquare.forEach((line) => console.log(line.join(' ').trim()));
+newSquare.forEach((line) => console.log(line.join(' ')));
