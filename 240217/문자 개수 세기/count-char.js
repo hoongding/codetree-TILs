0d460b89c0d22fdde8e92ff8e5af1a7628df77b5
Hoonseok <56246060+hoongding@users.noreply.github.com>
2024-02-17 22:00:34 +0900

    Title: 문자 개수 세기, Time: 174ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240217/문자 개수 세기/count-char.js
@@ -0,0 +1,14 @@
+const fs = require('fs')
+
+const inputs = fs.readFileSync(0).toString().trim();
+
+const string = inputs[0].split();
+const target = inputs[1];
+
+let cnt = 0;
+
+string.forEach((str) => {
+    if(str === target) cnt++
+})
+
+console.log(cnt);
