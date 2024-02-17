25815576e18f09f9c8537c5b87b5864cb1b78e47
Hoonseok <56246060+hoongding@users.noreply.github.com>
2024-02-17 22:01:37 +0900

    Title: 문자 개수 세기, Time: 304ms, Memory: 14MB, Status: Passed - CodeTree

--- 240217/문자 개수 세기/count-char.js
+++ 240217/문자 개수 세기/count-char.js
@@ -1,8 +1,8 @@
 const fs = require('fs')
 
-const inputs = fs.readFileSync(0).toString().trim();
+const inputs = fs.readFileSync(0).toString().trim().split('\n');
 
-const string = inputs[0].split();
+const string = inputs[0].split('');
 const target = inputs[1];
 
 let cnt = 0;
