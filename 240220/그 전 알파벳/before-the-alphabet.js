d7d04ced7131d312b4f401e5bb8c597d693495dd
Hoonseok <56246060+hoongding@users.noreply.github.com>
2024-02-20 19:30:45 +0900

    Title: 그 전 알파벳, Time: 238ms, Memory: 14MB, Status: Passed - CodeTree

--- 240220/그 전 알파벳/before-the-alphabet.js
+++ 240220/그 전 알파벳/before-the-alphabet.js
@@ -2,4 +2,5 @@ const fs = require('fs');
 
 const str = fs.readFileSync(0).toString().trim();
 
-console.log(String.fromCharCode(str.charCodeAt() - 1))
+if(str === 'a') console.log('z');
+else console.log(String.fromCharCode(str.charCodeAt() - 1))
