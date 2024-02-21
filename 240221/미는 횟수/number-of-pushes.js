e90f4b934d5700a7815d6b434ec5b968eff62cfd
Hoonseok <56246060+hoongding@users.noreply.github.com>
2024-02-21 20:34:48 +0900

    Title: 미는 횟수, Time: 204ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240221/미는 횟수/number-of-pushes.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+
+let [string, target] = fs.readFileSync(0).toString().trim().split('\n');
+
+const shiftRight = (arr) => {
+    return [...arr.slice(1), arr[0]].join('');
+}
+
+let n = 0;
+
+for(let i = 1; i <= string.length; i++){
+    string = shiftRight(string.split(''));
+    if(string === target){
+        n = i;
+        break;
+    }
+}
+
+console.log(n);
