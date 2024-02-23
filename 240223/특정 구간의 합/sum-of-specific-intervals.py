8718c1030429fecedcdb7b6e89325fc2d691551e
Hoonseok <56246060+hoongding@users.noreply.github.com>
2024-02-23 22:50:14 +0900

    Title: 특정 구간의 합, Time: 124ms, Memory: 25MB, Status: Passed - CodeTree

--- 240223/특정 구간의 합/sum-of-specific-intervals.py
+++ 240223/특정 구간의 합/sum-of-specific-intervals.py
@@ -1,16 +1,13 @@
 [n, m] = map(int, input().split())
-a_arr = map(int, input().split())
+a_arr = list(map(int, input().split()))
 
 pair_arr = [
     list(map(int, input().split()))
     for _ in range(m)
 ]
 
-print(pair_arr)
-
 def find_sum(start_idx, end_idx):
-    print(start_idx)
-    return sum(a_arr[start_idx:end_idx])
+    return sum(a_arr[start_idx - 1:end_idx])
 
 def find_answer():
     _sum = []
@@ -19,4 +16,5 @@ def find_answer():
     
     return _sum
 
-print(find_answer())
+for _sum in find_answer():
+    print(_sum)
