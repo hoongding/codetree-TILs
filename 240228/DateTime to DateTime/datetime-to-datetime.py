0fe56f88106b37ba83993f1daed0ba70046ffa72
Hoonseok <56246060+hoongding@users.noreply.github.com>
2024-02-28 19:41:16 +0900

    Title: DateTime to DateTime, Time: 121ms, Memory: 25MB, Status: Passed - CodeTree

--- 240228/DateTime to DateTime/datetime-to-datetime.py
+++ 240228/DateTime to DateTime/datetime-to-datetime.py
@@ -3,20 +3,21 @@ day, hour, minute = tuple(map(int, input().split()))
 if (day == 11 and hour < 11) or (day == 11 and hour == 11 and minute < 11):
     print(-1)
 
-cnt = 0
-t_d, t_h, t_m = (11, 11, 11)
-while True:
-    if t_d == day and t_h == hour and t_m == minute:
-        break
+else:
+    cnt = 0
+    t_d, t_h, t_m = (11, 11, 11)
+    while True:
+        if t_d == day and t_h == hour and t_m == minute:
+            break
     
-    cnt += 1
-    t_m += 1
+        cnt += 1
+        t_m += 1
 
-    if t_m == 60:
-        t_h += 1
-        t_m = 0
-    if t_h == 24:
-        t_d += 1
-        t_h = 0
+        if t_m == 60:
+            t_h += 1
+            t_m = 0
+        if t_h == 24:
+            t_d += 1
+            t_h = 0
 
-print(cnt)
+    print(cnt)
