f16a309d750dbc32ab0d2c7eec11bbab31c4a693
Hoonseok <56246060+hoongding@users.noreply.github.com>
2024-03-05 02:49:51 +0900

    Title: 연속되는 수 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240305/연속되는 수 2/continuous-number2.py
+++ 240305/연속되는 수 2/continuous-number2.py
@@ -6,8 +6,12 @@ nums = [
 ]
 
 cnt = 1
+cnts = []
 for i in range(n):
-    if i != 0 and nums[i] != nums[i - 1]:
+    if i != 0 and nums[i] == nums[i - 1]:
         cnt += 1
+    if i != 0 and nums[i] != nums[i - 1]:
+        cnts.append(cnt)
+        cnt = 1
 
-print(cnt)
+print(max(cnts))
