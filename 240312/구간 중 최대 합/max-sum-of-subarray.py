59e3973b3d510a5eb216a5a61087cd071b533ef7
Hoonseok <56246060+hoongding@users.noreply.github.com>
2024-03-12 20:58:08 +0900

    Title: 구간 중 최대 합, Time: 106ms, Memory: 25MB, Status: Passed - CodeTree

--- 240312/구간 중 최대 합/max-sum-of-subarray.py
+++ 240312/구간 중 최대 합/max-sum-of-subarray.py
@@ -5,6 +5,9 @@ nums = list(map(int, input().split()))
 
 max_sum = -sys.maxsize
 for i in range(n - k + 1):
-    max_sum = max(max_sum, nums[i] + nums[i + 1] + nums[i + 2])
+    cur_sum = 0
+    for j in range(k):
+        cur_sum += nums[i + j]
+    max_sum = max(max_sum, cur_sum)
 
 print(max_sum)
