61e448eff69a6fbe5c86b2809981fed089e70c51
Hoonseok <56246060+hoongding@users.noreply.github.com>
2024-03-14 21:52:58 +0900

    Title: G or H 2, Time: 142ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240314/G or H 2/G-or-H-2.py
@@ -0,0 +1,30 @@
+import sys
+n = int(input())
+
+people = [
+    tuple(input().split())
+    for _ in range(n)
+]
+
+positions = []
+for (position, _) in people:
+    positions.append(int(position))
+
+positions.reverse()
+
+max_size = - sys.maxsize
+for i in range(len(positions)):
+    for j in range(i, len(positions)):
+
+        start_point, end_point = i, j
+
+        g_cnt, h_cnt = 0, 0
+        for (position, alphabet) in people:
+            if positions[i] <= int(position) <= positions[j]:
+                if alphabet == 'G': g_cnt += 1
+                if alphabet == 'H': h_cnt += 1
+            
+        if g_cnt == h_cnt or (g_cnt > 0 and h_cnt == 0) or (h_cnt > 0 and g_cnt == 0):
+            max_size = max(max_size, positions[j] - positions[i])
+
+print(max_size)
