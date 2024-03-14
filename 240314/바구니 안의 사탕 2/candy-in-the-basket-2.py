ac26c2efbee772a3911a088745d19a1f8537a764
Hoonseok <56246060+hoongding@users.noreply.github.com>
2024-03-14 18:04:59 +0900

    Title: 바구니 안의 사탕 2, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240314/바구니 안의 사탕 2/candy-in-the-basket-2.py
@@ -0,0 +1,22 @@
+import sys
+# n개 바구니, 앞 뒤 k
+n, k = list(map(int, input().split()))
+
+# 사탕개수, 바구니 좌표
+baskets = [
+    tuple(map(int, input().split()))
+    for _ in range(n)
+]
+
+max_size = -sys.maxsize
+MAX_PLACE = 100
+
+for center in range(k + 1, 100 - k + 1):
+    cur_start, cur_end = center - k, center + k
+    cur_sum = 0
+    for candies, coor in baskets:
+        if cur_start <= coor <= cur_end:
+            cur_sum += candies
+    max_size = max(max_size, cur_sum)
+
+print(max_size)
