f061a79e62fcd3578cddbcea7c943048fb724a27
Hoonseok <56246060+hoongding@users.noreply.github.com>
2024-03-16 00:45:00 +0900

    Title: 밭의 높이를 고르게하기, Time: 116ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240316/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.py
@@ -0,0 +1,15 @@
+import sys
+n, h, t = tuple(map(int, input().split()))
+
+heights = list(map(int, input().split()))
+
+# 최소 t번 이상 h 높이만큼 맞추고 싶다. 어떻게 하면 좋을까?
+min_cost = sys.maxsize
+for i in range(n - t):
+    costs = 0
+    for idx in range(i, i + t):
+        costs += abs(h - heights[idx])
+    
+    min_cost = min(min_cost, costs)
+
+print(min_cost)
