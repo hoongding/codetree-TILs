81753cbbd08fbaef15bb04ebbbdbe5514667b3a2
Hoonseok <56246060+hoongding@users.noreply.github.com>
2024-03-22 14:56:34 +0900

    Title: 행복한 수열의 개수, Time: 114ms, Memory: 28MB, Status: Passed - CodeTree

--- 240322/행복한 수열의 개수/number-of-happy-sequence.py
+++ 240322/행복한 수열의 개수/number-of-happy-sequence.py
@@ -14,27 +14,33 @@ grid = [
 answer = 0
 for row in grid:
     cur_item = row[0]
+    max_cnt = 0
     cnt = 1
     for idx in range(1, n):
         if row[idx] == cur_item:
             cnt += 1
         else:
+            max_cnt = max(max_cnt, cnt)
             cnt = 1
             cur_item = row[idx]
-    if cnt >= m:
+    max_cnt = max(max_cnt, cnt)
+    if max_cnt >= m:
         answer += 1
     
 # 2. 세로 탐색
 for i in range(n):
     cur_item = grid[0][i]
     cnt = 1
+    max_cnt = 0
     for col in range(1, n):
         if grid[col][i] == cur_item:
             cnt += 1
         else:
+            max_cnt = max(max_cnt, cnt)
             cnt = 1
             cur_item = grid[col][i]
-    if cnt >= m:
+    max_cnt = max(max_cnt, cnt)
+    if max_cnt >= m:
         answer += 1
 
 print(answer)
