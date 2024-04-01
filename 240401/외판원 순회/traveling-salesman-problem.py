e1e585209639d31363c1e3e8e7f6af9c3aa6744f
Hoonseok <56246060+hoongding@users.noreply.github.com>
2024-04-01 12:28:16 +0900

    Title: 외판원 순회, Time: 454ms, Memory: 27MB, Status: Passed - CodeTree

---
+++ 240401/외판원 순회/traveling-salesman-problem.py
@@ -0,0 +1,45 @@
+import sys
+n = int(input())
+
+weights = [
+    list(map(int, input().split()))
+    for _ in range(n)
+]
+
+answer = []
+visited = [False for _ in range(n)]
+
+# 다시 1번으로 돌아와야함.
+def get_weights():
+    temp_sum = 0
+    for cur_grid, next_grid in answer:
+        temp_sum += weights[cur_grid][next_grid]
+
+    temp_sum += weights[answer[-1][1]][0]
+    
+    return temp_sum
+
+min_sum = sys.maxsize
+def choose(cur_num):
+    global min_sum
+    if len(answer) == n - 1:
+        if weights[answer[-1][1]][0] != 0:
+            min_sum = min(min_sum, get_weights())
+        return
+    
+    for i in range(1, n):
+        if visited[i]:
+            continue
+        if weights[cur_num][i] == 0:
+            continue
+
+        visited[i] = True
+        answer.append((cur_num, i))
+        choose(i)
+        answer.pop()
+        visited[i] = False
+
+visited[0] = True
+choose(0)
+
+print(min_sum)
